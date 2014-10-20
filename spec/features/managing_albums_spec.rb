815387f1d68b270ce3aec6ea8a5aea78aeebabf1
seanrlarge <sean.large1@gmail.com>
2014-10-20 16:21:40 -0400

    all test working. able to add new and update existing albums

--- spec/features/managing_albums_spec.rb
+++ spec/features/managing_albums_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
+def create_astrolounge
+  Album.create!(
+      title: 'Astrolounge',
+      artist: 'Smash Mouth',
+      year: '1999'
+      )
+end
+
 feature "Manage Albums" do
   scenario "List no Albums" do
     visit albums_path
@@ -8,11 +16,9 @@ feature "Manage Albums" do
 
   end
   scenario "List all Albums" do
-    Album.create!(
-      title: 'Astrolounge',
-      artist: 'Smash Mouth',
-      year: '1999'
-      )
+    create_astrolounge
+
+
     visit albums_path
     expect(page.find('.album')).to have_content(/Astrolounge/)
     expect(page.find('.artist')).to have_content(/Smash Mouth/)
@@ -20,18 +26,25 @@ feature "Manage Albums" do
     expect(page).not_to have_content(/No albums available/i)
   end
   scenario "Add new Album" do
-    Album.create(
-      title: 'Astrolounge',
-      artist: 'Smash Mouth',
-      year: '1999'
-      )
-    visit albums_path
-    expect(page.find('.album')).to have_content(/Astrolounge/)
-    expect(page.find('.artist')).to have_content(/Smash Mouth/)
-    expect(page.find('.year')).to have_content(/1999/)
-    expect(page).not_to have_content(/No albums available/i)
-  end
-  scenario "Update an Album"
+    visit new_album_path
+    fill_in 'Title', with: 'Astrolounge'
+    fill_in 'Artist', with: 'Smash Mouth'
+    fill_in 'Year', with: '1999'
+    click_on 'Create Album'
 
+    expect(current_path).to eq(album_path(Album.last))
+    expect(find('#notice')).to have_content(/Successfully/)
+  end
+  scenario "Update an Album" do
+    album = create_astrolounge
+    visit edit_album_path(album)
+
+    fill_in 'Title', with: 'Smash Mouth'
+    fill_in 'Year', with: '2001'
+    click_on 'Update Album'
+
+    expect(current_path).to eq(album_path(album))
+    expect(find('#notice')).to have_content(/updated/i)
+    end
 end
 
