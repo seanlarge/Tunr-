af99745d368a681a7f8e41eef859cf9f7fdef060
seanrlarge <sean.large1@gmail.com>
2014-10-20 12:09:47 -0400

    Test in place to see if albums is empty and to display NO Albums showing when empty

--- spec/features/managing_albums_spec.rb
+++ spec/features/managing_albums_spec.rb
@@ -17,6 +17,7 @@ feature "Manage Albums" do
     expect(page.find('.album')).to have_content(/Astrolounge/)
     expect(page.find('.artist')).to have_content(/Smash Mouth/)
     expect(page.find('.year')).to have_content(/1999/)
+    expect(page).not_to have_content(/No albums available/i)
   end
   scenario "Add new Album"
 end
