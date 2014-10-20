3ad2ad7a54e7f57d588aaff3f618c352e05597dd
seanrlarge <sean.large1@gmail.com>
2014-10-20 11:34:08 -0400

    changes to controller, views and spec to reflect test requirements

--- spec/features/managing_albums_spec.rb
+++ spec/features/managing_albums_spec.rb
@@ -7,6 +7,17 @@ feature "Manage Albums" do
     expect(page).to have_content(/No albums available/i)
 
   end
-  scenario "List all Albums"
+  scenario "List all Albums" do
+    Album.create!(
+      title: 'Astrolounge',
+      artist: 'Smash Mouth',
+      year: '1999'
+      )
+    visit albums_path
+    expect(page.find('.album')).to have_content(/Astrolounge/)
+    expect(page.find('.artist')).to have_content(/Smash Mouth/)
+    expect(page.find('.year')).to have_content(/1999/)
+  end
   scenario "Add new Album"
 end
+
