900711db196315016e538ad41be2b55a36e3f7c1
seanrlarge <sean.large1@gmail.com>
2014-10-24 09:42:20 -0400

    Songs controller and views uptodate however sessions is giving me a long in error when trying to run on local host

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,50 @@
 class SongsController < ApplicationController
   def new
+    @song = Song.new
   end
 
   def edit
+    @song = Song.find(params[:id])
   end
 
   def index
+    @song = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
+
+  def destroy
+    if @song.delete
+      redirect_to albums_path, notice: "Deleted!"
+    else
+      render albums_path, notice: "Error, please try again."
+    end
+  end
+
+  def update
+    @song = song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to @song, notice: "Successfully Updated"
+    else
+      render :edit
+    end
+  end
+
+  def create
+      @song = Song.new(band_params)
+      if @song.save
+          redirect_to @Song, notice: "Song Created!"
+      else
+          render :new
+      end
+    end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:title)
+  end
+end
 end
